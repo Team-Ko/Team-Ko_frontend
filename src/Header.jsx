493b39fd96f3599654f8d5842c5e2a303bf8ddc9
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-03-15 15:08:28 +0900

    Header 수정

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
     <div className="App">
         <header className="header">
-          <ul className="row">
+          <ul>
             <li><a href="#">타로여신 소개</a></li>
             <li><a href="#">이용안내</a></li>
             <li><a href="#">공지사항</a></li>
@@ -15,7 +15,7 @@ const Header = () => {
           <h1>
             <img src="/images/logo1.png" alt="로고"/>
           </h1>
-          <ul className="row">
+          <ul>
             <li><a href="#">상담사 소개</a></li>
             <li><a href="#">문의게시판</a></li>
             <li><a href="#">할인상담</a></li>
