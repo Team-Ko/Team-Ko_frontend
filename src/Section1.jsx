50c4436d7f70b6ef6badb19d00c5a496e4a7141f
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-03-15 15:08:42 +0900

    Section1 수정

--- src/Section1.jsx
+++ src/Section1.jsx
@@ -4,9 +4,9 @@ import './css/Section1.css';
 const Section1 = () => {
     return (
         <div className="section1">
-            <h2>정확한답변</h2>
-            <h3>TAROT GODDESS</h3>
-            <p>타로여신의 상담사들은 지정된 전문교육기관에서 교육을 마친 심리상담사들로 구성되어 있습니다.</p>
+            <h2 className="section1_h2">정확한답변</h2>
+            <h3 className="section1_h3">TAROT GODDESS</h3>
+            <p className="section1_p">타로여신의 상담사들은 지정된 전문교육기관에서 교육을 마친 심리상담사들로 구성되어 있습니다.</p>
         </div>
     );
 }
