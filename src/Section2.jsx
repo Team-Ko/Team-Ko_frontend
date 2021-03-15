be52e370ebbb95178a47a7ab870af577fb42e715
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-03-15 15:07:46 +0900

    Section2

--- src/Section2.jsx
+++ src/Section2.jsx
@@ -1,14 +1,16 @@
-import './css/Section1.css';
+import './css/Section2.css';
 
 //세션2 컴포넌트
 const Section2 = () => {
     return (
         <div className="section2">
             <div className="left">
-
+            <img className="section2_img" src="/images/cat.png" alt="고양이"/>
             </div>
             <div className="right">
-                
+                <h2 className="section2_h2">신동현의 코는 진짜인가</h2>
+                <p className="section2_p">신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다
+                신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다</p>
             </div>
         </div>
     );
