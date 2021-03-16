f8b359b4bbffd78b0306113f5eaf6c0ae1aab25a
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-03-16 16:03:26 +0900

    Section2 버튼 추가

--- src/Section2.jsx
+++ src/Section2.jsx
@@ -11,6 +11,17 @@ const Section2 = () => {
                 <h2 className="section2_h2">신동현의 코는 진짜인가</h2>
                 <p className="section2_p">신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다
                 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다 신동현의 코는 진짜이다</p>
+                <div className="section2_box">
+                    <div className="section2_btn1">
+                        <a href="http://tarogoddess.co.kr/new/sub/about.html">타로여신</a>
+                    </div>
+                    <div className="section2_btn1">
+                        <a href="http://tarogoddess.co.kr/new/sub/guide.html">이용안내</a>
+                    </div>
+                    <div className="section2_btn2">
+                        <a href="http://tarogoddess.co.kr/taro/save_new.php">할인상담</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
