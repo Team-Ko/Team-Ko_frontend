58c24e66a77950478d4c435df0ec6ce7fefb27bc
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-04-06 14:08:31 +0900

    Create Header.jsx

---
+++ src/Component/Header.jsx
@@ -0,0 +1,55 @@
+import React, {} from 'react';
+import styled from 'styled-components';
+import Image from '../Images/TG삼보.png';
+
+const Container = styled.div`
+    width : 100%;
+    height : 170px;
+    text-align: center;
+`;
+
+const Img = styled.div`
+    width : 100%;
+    height : 120px;
+    background-color: white;
+    margin : 0px auto;
+`
+
+const Menu = styled.div`
+    width : 100%;
+    height : 50px;
+    background-color: #707070;
+    display : flex;
+`;
+
+const MenuButton = styled.div`
+    width : 280px;
+    height: 50px;
+    color : white;
+    font-size : 20px;
+    line-height : 50px;
+    &:hover {
+        background-color : #00a7e1;
+        color : black;
+    }
+`;
+
+const Header = () => {
+    return(
+        <Container>
+            <Img>
+            <img src={Image} alt="로고"/>
+            </Img>
+            <Menu>
+                <MenuButton style={{marginLeft:"110px"}}>회사소개</MenuButton>
+                <MenuButton>제품소개</MenuButton>
+                <MenuButton>사업분야</MenuButton>
+                <MenuButton>공지사항</MenuButton>
+                <MenuButton>고객지원</MenuButton>
+                <MenuButton>메일문의</MenuButton>
+            </Menu>
+        </Container>
+    );
+}
+
+export default Header;
