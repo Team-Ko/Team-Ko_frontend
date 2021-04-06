82aed7b8bcdaf06a75f2a22e864aad5b3f0c4cd8
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-04-06 12:55:32 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import './App.css';
-import Header from './Header';
-import Section1 from './Section1';
-import Section2 from './Section2';
-import Card from './Card';
-import Footer from './Footer';
-import Copyright from './Copyright';
+
 
 function App() {
   return (
-    <div className="warp">
-      < Header />
-      < Section1 />
-      < Section2 />
-      <div className="card_warp">
-        < Card/>
-        < Card/>
-        < Card/>
-      </div>
-      <div className="card_warp">
-        < Card/>
-        < Card/>
-        < Card/>
-      </div>
-      < Footer/>
-        <img className="footer_logo" src="/images/logo1.png" alt="로고"/>
-      < Copyright/>
+    <div>
+
     </div>
   );
 }
