b4919b5de576867394e4d683f996100062a3c48e
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-04-08 13:55:25 +0900

    슬라이드 추가

--- src/Component/Slide.jsx
+++ src/Component/Slide.jsx
@@ -1,6 +1,14 @@
 import React, {} from 'react';
 import styled from 'styled-components';
 import Slide1 from '../Images/slide1.png';
+import Slide2 from '../Images/slide2.png';
+import Slide3 from '../Images/slide3.png';
+import Slide4 from '../Images/slide4.png';
+import Slide5 from '../Images/slide5.png';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Container = styled.div`
     width: 100%;
@@ -8,13 +16,41 @@ const Container = styled.div`
     overflow : hidden;
     text-align: center;
     background-color : #DFE1E5;
+    width: 100%;
 `;
+
+
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    arrows: true,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+    appendDots: (dots) => (
+        <div
+          style={{
+            padding: "50px",
+          }}
+        >
+          <ul style={{ margin: "0px" }}> {dots} </ul>
+        </div>
+      ),
+    };
 
 const Slide = () => {
     return(
-        <Container>
-            <img src={Slide1} alt="모니터"/>
-        </Container>
+            <Slider {...settings}>
+                <Container><img src={Slide1} alt="모니터"/></Container>
+                <Container><img src={Slide2} alt="모니터"/></Container>
+                <Container><img src={Slide3} alt="모니터"/></Container>
+                <Container><img src={Slide4} alt="모니터"/></Container>
+                <Container><img src={Slide5} alt="모니터"/></Container>
+            </Slider>
     );
 }
 
