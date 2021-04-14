f75fe4b81896803a7b17fc9a5265b9373cffb43f
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-04-14 12:00:40 +0900

    로고,이미지 추가

--- src/Component/Helper.jsx
+++ src/Component/Helper.jsx
@@ -1,6 +1,9 @@
 import React, {} from 'react';
 import styled from 'styled-components';
-import a from '../Images/a.png';
+import driver from '../Images/driver.png';
+import technics from '../Images/technics.png';
+import ebook from '../Images/ebook.png';
+import trophy from '../Images/trophy.png';
 
 const Container = styled.div`
     width : 100%;
@@ -31,25 +34,25 @@ const Img = styled.img`
 const Helper = () => {
     const List = [
         {
-            img : a,
+            img : driver,
             title : '드라이버 다운로드',
             content : '드라이버를 다운로드',
             content_2 : '하실 수 있습니다.'
         },
         {
-            img : a,
+            img : technics,
             title : 'A/S 센터 위치',
             content : '가까운 TG 서비스센터를',
             content_2 : '찾아 보실 수 있습니다.'
         },
         {
-            img : a,
+            img : ebook,
             title : '웹 카탈로그',
             content : 'E-Book으로 편리하게',
             content_2 : '제품 소개 자료를 보실 수 있습니다.'
         },
         {
-            img : a,
+            img : trophy,
             title : '수상경력',
             content : 'TG삼보의 수상기록들을',
             content_2 : '보실 수 있습니다'
